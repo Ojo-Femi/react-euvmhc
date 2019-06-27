d358382eadb828addbca2518dff3cb3a9808626d
Ojo-Femi <ojokotor@gmail.com>
2019-06-27 09:53:21 +0100

    Initial commit

---
+++ Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Navbar extends Component {
+  render () {
+    return (
+      <ul>
+        <li><a href="#">Validate</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    )
+  }
+}
